ae2eb7f1fb72bcf58e5b1c059da39bec6aed14c3
ilya.vlasov <ilya.vlasov@ledgerleopard.com>
2020-11-23 08:25:47 +0300

    feat: some fixes for github action for publish

--- src/test/java/com/tease/androidlibraryactionstest/HelloMessageFactoryTest.java
+++ src/test/java/com/tease/androidlibraryactionstest/HelloMessageFactoryTest.java
@@ -1,19 +1,23 @@
 package com.tease.androidlibraryactionstest;
 
 import static org.junit.jupiter.api.Assertions.*;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 
-class HelloMessageFactoryTest {
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+
+@RunWith(JUnit4.class)
+public class HelloMessageFactoryTest {
     private HelloMessageFactory factory;
 
-    @BeforeEach
-    void setUp() {
+    @Before
+    public void setUp() {
         factory = new HelloMessageFactory("Hello world!");
     }
 
     @Test
-    void message() {
+    public void message() {
         String message = factory.message();
         assertEquals(message, "Hello world!");
     }
